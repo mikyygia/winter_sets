5ef930bae2b7744c42f538058fadfc94055b6322
yun <contactinfo.nhi@gmail.com>
2024-02-22 17:17:55 -0800

    Base and derived class

--- problem3.py
+++ problem3.py
@@ -8,3 +8,12 @@ class Person:
             return False
 
         return (self.name == other.name) and (self.age == other.age)
+
+
+class Student(Person):
+    def __init__(self, name, age, major) -> None:
+        super().__init__(name, age)
+        self.major = major
+
+    def __eq__(self, other: object) -> bool:
+        return (super().__eq__(other)) and (self.major == other.major)
